228392ce5d2fe1ada669bc741d0884653a3174a8
Andreea <andreea.stegerean@x2mobile.net>
2018-08-24 13:13:47 +0300

    Initial Commit

---
+++ myFirstFramework/myFirstFramework.h
@@ -0,0 +1,19 @@
+//
+//  myFirstFramework.h
+//  myFirstFramework
+//
+//  Created by Andreea Stegerean on 24/08/2018.
+//  Copyright © 2018 andreea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myFirstFramework.
+FOUNDATION_EXPORT double myFirstFrameworkVersionNumber;
+
+//! Project version string for myFirstFramework.
+FOUNDATION_EXPORT const unsigned char myFirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myFirstFramework/PublicHeader.h>
+
+
