c79eb281ecb2b13aecd9513b67873539566faf8b
Andreea <andreea.stegerean@x2mobile.net>
2018-08-24 13:16:57 +0300

    added Hello message

---
+++ myFirstFramework/MtFirstWelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MtFirstWelcomeViewController.swift
+//  myFirstFramework
+//
+//  Created by Andreea Stegerean on 24/08/2018.
+//  Copyright © 2018 andreea. All rights reserved.
+//
+
+import UIKit
+
+class MtFirstWelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
